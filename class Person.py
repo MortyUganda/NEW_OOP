7729f22373804674e3616f9d803f61df88ff20aa
Sergei <114567024+MortyUganda@users.noreply.github.com>
2025-09-29 16:54:54 +0300

    ignored

--- class Person.py
+++ class Person.py
@@ -1,3 +1,16 @@
+# Implement a Person class that describes a person. When creating an instance, the class should accept two arguments in the following order:
+
+# name — the person's name
+# surname — the person's last name
+# An instance of the Person class should have two attributes:
+
+# name — the person's name
+# surname — the person's last name
+# The Person class should have one property:
+
+# fullname — a read-write property that returns the person's full name as a string: 
+# <name> <last name>
+
 class Person:
     def __init__(self, name, surname) -> None:
         self.name = name
@@ -10,3 +23,13 @@ class Person:
         self.name, self.surname = info.split()
 
     fullname = property(get_name, set_name)
+
+person = Person('Меган', 'Фокс')
+
+print(person.name)
+print(person.surname)
+print(person.fullname)
+
+# Меган
+# Фокс
+# Меган Фокс
