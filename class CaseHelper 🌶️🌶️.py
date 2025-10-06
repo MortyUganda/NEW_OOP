e66188ec75f984d5fc688cfd49b11dfc6cb8f639
Sergei <114567024+MortyUganda@users.noreply.github.com>
2025-10-06 22:10:48 +0300

    tests

---
+++ class CaseHelper 🌶️🌶️.py
@@ -0,0 +1,14 @@
+class CaseHelper :
+    @staticmethod
+    def is_snake(stroka):
+        pass
+    @staticmethod
+    def is_upper_camel(stroka):
+        pass
+    @staticmethod
+    def to_snake(stroka):
+        pass
+    @staticmethod
+    def to_upper_camel(stroka):
+        pass
+    
